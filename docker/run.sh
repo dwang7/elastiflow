82e4cb1c6647506ec11cd2a1a75283e303a84d7f
empty <empty@arubanetworks.com>
2018-08-29 10:16:59 -0700

    get Dockers to work on docker version 1.7.1

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-docker network create elastic
+# docker network create elastic
 
 docker rm -f elasticsearch
 docker rm -f logstash
 docker rm -f kibana
 
-docker run -d --network=elastic --name=elasticsearch elasticsearch:latest
-docker run -d --network=elastic --name=logstash logstash:latest
-docker run -d --network=elastic --name=kibana -p 5601:5601 kibana:latest
+HOSTNAME=`hostname -i`
+
+docker run -d  --name=elasticsearch  elasticsearch:latest
+docker run -d  --name=logstash --link elasticsearch -p "$HOSTNAME:6343:6343/udp" logstash:latest
+docker run -d  --name=kibana --link elasticsearch -p 5601:5601 kibana:latest
 
 echo "Waiting for kibana server to be up before pushing the templates..."
 
